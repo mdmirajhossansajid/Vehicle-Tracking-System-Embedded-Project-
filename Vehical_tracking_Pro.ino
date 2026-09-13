f4a1f5272995127bc2e9bb594ffc0b44bc101ec8
Miraj Hossan Sajid <mdmirajhossansajidpc@gmail.com>
2026-09-13 12:10:54 +0600

    Add initial vehicle tracking setup code

---
+++ Vehical_tracking_Pro.ino
@@ -0,0 +1,53 @@
+#include <WiFi.h>
+#include <FirebaseESP32.h>
+#include <TinyGPS++.h>
+#include <HardwareSerial.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SH110X.h> 
+
+// --- EXTRACTED CREDENTIAL CONFIG ---
+#define WIFI_SSID "technolab"
+#define WIFI_PASSWORD "12345678"
+
+#define DATABASE_URL "xxxxxxxxxxxxxxxxxxxxxxxxxx"
+#define API_KEY "xxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+
+// --- PIN CONFIGURATION ---
+#define RXD2 16
+#define TXD2 17
+#define i2c_Address 0x3c 
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET -1
+
+// --- OBJECTS ---
+TinyGPSPlus gps;
+HardwareSerial SerialGPS(2); 
+Adafruit_SH1106G display = Adafruit_SH1106G(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+FirebaseData firebaseData;
+FirebaseConfig config;
+FirebaseAuth auth;
+FirebaseJson json;
+
+unsigned long lastUpdateTime = 0;
+
+void setup() {
+
+    Serial.begin(115200);
+  SerialGPS.begin(9600, SERIAL_8N1, RXD2, TXD2);
+
+  // Initialize 1.3" OLED Display unit
+  display.begin(i2c_Address, true); 
+  display.clearDisplay();
+  display.setTextColor(SH110X_WHITE);
+  display.setTextSize(1);
+
+}
+void loop() {
+
+}
+void updateSystem() {
+    
+}
